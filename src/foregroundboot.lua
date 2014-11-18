bacbd09e0c6f5c2fe9795bbf31f926ff6cdcfd92
Dan Block (macbook) <blockda@gmail.com>
2014-11-17 21:08:19 -0800

    Migrating code from the BlowTorch repository.

---
+++ src/foregroundboot.lua
@@ -0,0 +1,15 @@
+--debugPrint("package path:"..package.path)
+
+require("serialize")
+--make a button.
+--debugPrint("HEY HEY HEY LOOK HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nsfsafasf\nin the ui bootstrap!!!!!")
+
+
+dofile(GetPluginInstallDirectory().."/layout_manager_ui.lua")
+
+
+
+
+
+
+
