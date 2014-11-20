1f26d0f6bfa11ada938f47907cbb0ff7ac014025
Dan Block (macbook) <blockda@gmail.com>
2014-11-20 15:05:33 -0800

    adding options to the layout manager, need a function in the core to query a settings value from the window. I think this is reasonable.

--- src/backgroundboot.lua
+++ src/backgroundboot.lua
@@ -33,5 +33,39 @@ function LoadLayout(layout)
 end
 --NewWindow("lua_window",880,577,400,100,"windowscript")
 
+function setScrollContainerWidth(value)
+  options.scroll_contianer_width = value
+  WindowXCallS("layout_manager","SetScrollContainerWidth",value)
+end
+
+function setBottomContainerHeight(value)
+  options.bottom_container_heght = value;
+  WindowXCallS("layout_manager","SetBottomContainerHeight",value)
+end
+
+function setLayoutFromPreferences(value)
+  WindowXCallS("layout_manager","LoadLayout",value + 1)
+end
+
+optionsTable = {}
+optionsTable.scroll_container_width = setScrollContainerWidth
+optionsTable.bottom_container_height = setBottomContainerHeight
+optionsTable.selected_layout = setLayoutFromPreferences
+
+
+options = {}
+options.scroll_container_width = 150
+options.bottom_container_height = 70
+options.selected_layout = 3
+
+function OnOptionChanged(key,value)
+  --Note("\n"..key..":"..value.."\n")
+  local func = optionsTable[key]
+  if(func ~= nil) then
+    func(value)
+  end
+  
+end
+
 
 --debugPrint("startup script loaded!")
